b8359356e038937d257f57ba3a7000ef5fe07d64
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2025-11-05 21:59:45 -0500

    enable audio

--- scripts/config.lua
+++ scripts/config.lua
@@ -112,14 +112,11 @@ settings.joy_threshold = 0.10
 
 -- audio
 -- settings.tracks = { }
-settings.current_track = "revenge_of_cats.it"
+settings.current_track = "song_revenge_of_cats.it"
 -- music & fx settings
-settings.loopMusic = 0
--- TODO: debug setting 
-settings.playEffects = 0
-settings.playMusic = 0
--- settings.playEffects = 1
--- settings.playMusic = 1
+settings.loopMusic = 1
+settings.playEffects = 1
+settings.playMusic = 1
 settings.fxVolume = 0.2
 settings.musicVolume = 0.5
 
--- src/audio/sound_glue.cpp
+++ src/audio/sound_glue.cpp
@@ -1,4 +1,4 @@
-#if 0
+#if 1
 #include "audio/sound_glue.h"
 extern "C" {
 #include "game/game.h"
